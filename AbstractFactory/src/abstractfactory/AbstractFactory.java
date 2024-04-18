f18f967f6d2dc404ecae498000aca6e564cbaf62
chris2500 <chrisvasquez985@gmail.com>
2024-04-17 23:21:04 -0600

    Paquetes patrones de disenio

---
+++ AbstractFactory/src/abstractfactory/AbstractFactory.java
@@ -0,0 +1,16 @@
+package abstractfactory;
+
+/**
+ *
+ * @author chris
+ */
+public class AbstractFactory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Builder/src/builder/Builder.java
@@ -0,0 +1,16 @@
+package builder;
+
+/**
+ *
+ * @author chris
+ */
+public class Builder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
