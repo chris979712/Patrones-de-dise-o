444d90269d489ce19d2e86b14250dc3f63ccabeb
chris2500 <chrisvasquez985@gmail.com>
2024-04-21 11:14:43 -0600

    Correccion de builders

--- Builder/src/builder/builders/KnightCharacterBuilder.java
+++ Builder/src/builder/builders/KnightCharacterBuilder.java
@@ -8,45 +8,54 @@ public class KnightCharacterBuilder extends CharacterBuilder{
 
     @Override
     public void build() {
-         this.knight = new Character();
+        System.out.println("Construyendo personaje...");
+        this.knight = new Character();
     }
 
     @Override
     public Character getCharacter() {
+        System.out.println("¡El caballero ha sido creado!");
         return this.knight;
     }
 
     @Override
     public void setAgility(String agility) {
+        System.out.println("Por su tamaño debe ser alguien lento");
         this.knight.setAgility(agility);
     }
     @Override
     public void setArmor(String armor) {
+        System.out.println("La armadura de hierro ha sido asignada");
         this.knight.setArmor(armor);
     }
 
     @Override
     public void setHealth(int health) {
+        System.out.println("La salud del caballero es insuperable");
         this.knight.setHealth(health);
     }
 
     @Override
     public void setRace(String race) {
+        System.out.println("El caballero pertenece a la raza de los gigantes");
         this.knight.setRace(race);
     }
 
     @Override
     public void setStrength(int strength) {
+        System.out.println("Su fuerza es la de 20 hombres");
         this.knight.setStrength(strength);
     }
 
     @Override
     public void setType(String type) {
+        System.out.println("Este personaje es un caballero");
         this.knight.setType(type);
     }
 
     @Override
     public void setWeapon(String weapon) {
+        System.out.println("La espada de hierro ha sido asignada");
         this.knight.setWeapon(weapon);
     }
     
--- Builder/src/builder/builders/WizzardCharacterBuilder.java
+++ Builder/src/builder/builders/WizzardCharacterBuilder.java
@@ -7,31 +7,37 @@ public class WizzardCharacterBuilder extends CharacterBuilder{
 
     @Override
     public void build() {
-         this.wizzard = new Character();
+        System.out.println("Construyendo personaje...");
+        this.wizzard = new Character();
     }
 
     @Override
     public Character getCharacter() {
+        System.out.println("¡El Mago ha sido creado!");
         return this.wizzard;
     }
 
     @Override
     public void setHealth(int health) {
+        System.out.println("La salud es ???");
         this.wizzard.setHealth(health);
     }
 
     @Override
     public void setRace(String race) {
+        System.out.println("Se dice que no es de este mundo...");
         this.wizzard.setRace(race);
     }
 
     @Override
     public void setStrength(int strength) {
+        System.out.println("Su fuerza se debe a la cantidad de energía que maneja");
         this.wizzard.setStrength(strength);
     }
 
     @Override
     public void setType(String type) {
+        System.out.println("Este tipo de personaje es un mago");
         this.wizzard.setType(type);
     }
 
