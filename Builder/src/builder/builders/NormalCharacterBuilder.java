c25f142fecde8726ae1b5dd690a422a6c1f3fc70
chris2500 <chrisvasquez985@gmail.com>
2024-04-21 11:14:29 -0600

    Correccion de builders

--- Builder/src/builder/builders/NormalCharacterBuilder.java
+++ Builder/src/builder/builders/NormalCharacterBuilder.java
@@ -8,26 +8,31 @@ public class NormalCharacterBuilder extends CharacterBuilder {
 
     @Override
     public void build() {
+        System.out.println("Construyendo personaje...");
          this.normal = new Character();
     }
 
     @Override
     public Character getCharacter() {
+        System.out.println("¡El personaje normal ha sido creado!");
         return this.normal;
     }
 
     @Override
     public void setHealth(int health) {
+        System.out.println("La salud es demasiado baja");
         this.normal.setHealth(health);
     }
 
     @Override
     public void setRace(String race) {
+        System.out.println("Este personaje pertenece a la raza humana");
         this.normal.setRace(race);
     }
     
     @Override
     public void setType(String type) {
+        System.out.println("Este personaje es uno normal");
         this.normal.setType(type);
     }
 }
