bb0f535876e52f5b64ab837cd9482617b671d521
chris2500 <chrisvasquez985@gmail.com>
2024-04-21 10:57:15 -0600

    Validacion de datos

--- Builder/src/builder/builders/CharacterBuilder.java
+++ Builder/src/builder/builders/CharacterBuilder.java
@@ -1,14 +1,27 @@
 package builder.builders;
 import builder.classes.Character;
 
-public interface CharacterBuilder {
-    public void build();
-    public Character getCharacter();
-    public void setAgility(String agility);
-    public void setArmor(String armor);
-    public void setHealth(int health);
-    public void setRace(String race);
-    public void setStrength(int strength);
-    public void setType(String type);
-    public void setWeapon(String weapon);
+public abstract class CharacterBuilder {
+    private Character character;
+    
+    public void build(){
+        character = new Character();
+    }
+    public Character getCharacter(){
+        return this.character;
+    }
+    public void setAgility(String agility){
+    }
+    public void setArmor(String armor){
+    }
+    public void setHealth(int health){
+    }
+    public void setRace(String race){
+    }
+    public void setStrength(int strength){
+    }
+    public void setType(String type){
+    }
+    public void setWeapon(String weapon){
+    }
 }
